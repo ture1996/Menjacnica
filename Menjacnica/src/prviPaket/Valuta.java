af8af72aabb7de2e13f663ddb95fc3f180bcb376
ture1996 <turanjanin1996@gmail.com>
2017-04-06 15:16:13 +0200

    Napravljena novi paket sa klasom
    Napravljen paket prviPaket
    
    Napravljena klasa Valuta sa atributima naziv, skraceniNaziv, datum i
    kurs
    Generisane get i set metode za sve atribute

---
+++ Menjacnica/src/prviPaket/Valuta.java
@@ -0,0 +1,39 @@
+package prviPaket;
+
+import java.util.GregorianCalendar;
+
+public class Valuta {
+	
+	private String naziv;
+	private String skraceniNaziv;
+	private GregorianCalendar datum;
+	private double[] kurs = new double[3];
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	public double[] getKurs() {
+		return kurs;
+	}
+	public void setKurs(double[] kurs) {
+		this.kurs = kurs;
+	}
+	
+	
+	
+}
