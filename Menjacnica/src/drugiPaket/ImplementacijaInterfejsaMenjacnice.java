f0e3bb412916113476da74dda092333bd38fbf2b
ture1996 <turanjanin1996@gmail.com>
2017-04-06 15:59:55 +0200

    Implementiran interfejs na master grani
    Na drugaciji nacin

--- Menjacnica/src/drugiPaket/ImplementacijaInterfejsaMenjacnice.java
+++ Menjacnica/src/drugiPaket/ImplementacijaInterfejsaMenjacnice.java
@@ -1,19 +1,41 @@
 package drugiPaket;
 
+import java.util.LinkedList;
+
 import prviPaket.Valuta;
 
 public class ImplementacijaInterfejsaMenjacnice implements InterfejsMenjacnice {
-
+	
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
+	
+	public void daLiJeNull(Valuta v){
+		if(v==null)
+			throw new RuntimeException("Parametar ne moze biti null!");
+	}
+	
 	public void dodaj(Valuta v) {
-
+		daLiJeNull(v);
+		if(valute.contains(v))
+			throw new RuntimeException("Vec postoji kurs date valute za ovaj datum!");
+		valute.add(v);
 	}
 
 	public void obrisi(Valuta v) {
-
+		daLiJeNull(v);
+		if(!valute.contains(v))
+			throw new RuntimeException("Ne postoji kurs date valute za ovaj datum!");
+		valute.remove(v);
 	}
 
 	public String pronadji(Valuta v) {
-		return null;
+		daLiJeNull(v);
+		if(!valute.contains(v))
+			throw new RuntimeException("Kurs koji ste uneli ne postoji!");
+		return v.toString();
 	}
 
+	public LinkedList<Valuta> getValute() {
+		return valute;
+	}
+	
 }
