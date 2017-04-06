d32e89cc70ee1abf80da42ab5718ba8e14238331
ture1996 <turanjanin1996@gmail.com>
2017-04-06 15:23:00 +0200

    Generisane hashCode, toString i equals metode

--- Menjacnica/src/prviPaket/Valuta.java
+++ Menjacnica/src/prviPaket/Valuta.java
@@ -1,5 +1,6 @@
 package prviPaket;
 
+import java.util.Arrays;
 import java.util.GregorianCalendar;
 
 public class Valuta {
@@ -32,6 +33,49 @@ public class Valuta {
 	}
 	public void setKurs(double[] kurs) {
 		this.kurs = kurs;
+	}
+	
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + Arrays.hashCode(kurs);
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+	
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (!Arrays.equals(kurs, other.kurs))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skraceniNaziv == null) {
+			if (other.skraceniNaziv != null)
+				return false;
+		} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+			return false;
+		return true;
+	}
+	
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv + ", datum=" + datum + ", kurs="
+				+ Arrays.toString(kurs) + "]";
 	}
 	
 	
