c25dff58b925497b6e383f8c49e6aa05067c27e3
ture1996 <turanjanin1996@gmail.com>
2017-04-06 15:43:11 +0200

    Napravljena klasa za implementaciju interfejsa u drugiPaket
    klasa ImplementacijaInterfejsaMenjacnice

---
+++ Menjacnica/src/drugiPaket/ImplementacijaInterfejsaMenjacnice.java
@@ -0,0 +1,19 @@
+package drugiPaket;
+
+import prviPaket.Valuta;
+
+public class ImplementacijaInterfejsaMenjacnice implements InterfejsMenjacnice {
+
+	public void dodaj(Valuta v) {
+
+	}
+
+	public void obrisi(Valuta v) {
+
+	}
+
+	public String pronadji(Valuta v) {
+		return null;
+	}
+
+}
